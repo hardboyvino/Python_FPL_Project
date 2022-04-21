0d94b8e8a94a0e0ddfcac7d14be1c73ca6235554
hardboyvino <97181625+hardboyvino@users.noreply.github.com>
2022-04-21 12:12:59 +0100

    Solved scrapping data & future gw

--- slider.py
+++ slider.py
@@ -17,6 +17,7 @@ def main():
     total_gw = int(input("How many GWs so far: "))
     pixels_per_gw = slider_width / (total_gw - 1)
     gws_to_consider = 2
+    predict_lookahead = 3 # How many GWs to use in prediction points scrapping
 
     # Identify the sliders
     slider_1 = driver.find_element(by=By.XPATH, value='//*[@id="__next"]//div[2]/div[2]/span/span[3]/span')
@@ -32,20 +33,136 @@ def main():
     time.sleep(5) # Because FFHub website is slow as balls for both sliders to move together
     ActionChains(driver).drag_and_drop_by_offset(slider_2, (gws_to_consider * pixels_per_gw), 0).perform() # Then move the upper limit to GW3
     time.sleep(5) # Because FFHub website is slow as balls for both sliders to move together
-    move_slider(slider_1, slider_2, pixels_per_gw, gws_to_consider)
+    move_slider(slider_1, slider_2, pixels_per_gw, gws_to_consider, predict_lookahead)
 
-def move_slider(slide1, slide2, pixels, gw):
+def move_slider(slide1, slide2, pixels, gw, predict):
     current_gw = "1" # Initialise to 1
 
     while current_gw < "5":
-        # Move lower slider to appropraite GW for the scrapping
-        ActionChains(driver).drag_and_drop_by_offset(slide1, pixels, 0).perform()
+        # Get Key Stats Page
+        stat_type = driver.find_element(by=By.XPATH, value='//*[@id="stattype"]/option[1]').click()
+
+        time.sleep(10)
+
+        # Get all the rows for Key Stats
+        playerNames = driver.find_elements(by=By.XPATH, value='//tbody/tr/td/div/a')
+        playerTeams = driver.find_elements(by=By.XPATH, value='//tbody/tr/td/div/div/span')
+        playerPrice = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[2]')
+        playerAppearances = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[3]')
+        playerMinutes = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[4]')
+        playerShot = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[5]')
+        playerOT = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[6]')
+        playerIn = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[7]')
+        playerBC = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[8]')
+        playerxG = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[9]')
+        playerG = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[10]')
+        playerPercentxGI = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[11]')
+        playerPercentGI = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[12]')
+        playerxGI = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[13]')
+        playerGI = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[14]')
+        playerKP = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[15]')
+        playerBCC = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[16]')
+        playerxAssist = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[17]')
+        playerAssists = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[18]')
+        playerxPts = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[19]')
+        playerBPS = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[20]')
+        playerB = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[21]')
+        playerPts = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[22]')
+
+        # Create the fpl_data dictionary
+        fpl_data = []
+
+        # For the length of all the players available, put them in the fpl_data dictionary. But I have realised you can run calculations straight from here as well which is AMAZING!!!
+        for i in range(len(playerNames)):
+            temp_data = {
+                        'Name': playerNames[i].text,
+                        'Team Name': playerTeams[i].text,
+                        'Price': float(playerPrice[i].text.replace("£","")),
+                        'Minutes': float(playerMinutes[i].text),
+                        'Appearances': playerAppearances[i].text,
+                        'Shots': float(playerShot[i].text),
+                        'On Target': float(playerOT[i].text),
+                        'Shots in Box': float(playerIn[i].text),
+                        'Big Chances': float(playerBC[i].text),
+                        'Expected Goals': float(playerxG[i].text),
+                        'Goals': float(playerG[i].text),
+                        '%%xGI': (float(playerPercentxGI[i].text.replace("%",''))) / 100,
+                        '%%GI': (float(playerPercentGI[i].text.replace("%",''))) / 100,
+                        'xGI': float(playerxGI[i].text),
+                        'Goal Involvement': float(playerGI[i].text),
+                        'Key Passes': float(playerKP[i].text),
+                        'Big Chances Created': float(playerBCC[i].text),
+                        'Expected Assists': float(playerxAssist[i].text),
+                        'Assists': float(playerAssists[i].text),
+                        'Expected Points': float(playerxPts[i].text),
+                        'BPS': float(playerBPS[i].text),
+                        'Bonus Points': float(playerB[i].text),
+                        'Points': float(playerPts[i].text)
+            }
+            fpl_data.append(temp_data)
+
+        time.sleep(10)
+
+        # Move the sliders forward for the predict scrapping
+        ActionChains(driver).drag_and_drop_by_offset(slide2, (pixels * predict), 0).perform()
         time.sleep(5)
-        ActionChains(driver).drag_and_drop_by_offset(slide2, pixels, 0).perform()
+        ActionChains(driver).drag_and_drop_by_offset(slide1, (pixels * predict), 0).perform()
         time.sleep(5)
 
-        # Get the current GW
+
+
+        # Get the future points
+        playerNames = driver.find_elements(by=By.XPATH, value='//tbody/tr/td/div/a')
+        playerPtsPredict = driver.find_elements(by=By.XPATH, value='//tbody/tr/td[22]')
+
+        # Create the fpl_data dictionary
+        fpl_data2 = []
+
+        # For the length of all the players available, put them in the fpl_data dictionary. But I have realised you can run calculations straight from here as well which is AMAZING!!!
+        for i in range(len(playerNames)):
+            temp_data2 = {
+                        'Name': playerNames[i].text,
+                        'PointsPredict': float(playerPtsPredict[i].text)
+            }
+            fpl_data2.append(temp_data2)        
+        time.sleep(10)
+
+        # Move sliders back to original positon
+        ActionChains(driver).drag_and_drop_by_offset(slide1, -(pixels * predict), 0).perform()
+        time.sleep(5)
+        ActionChains(driver).drag_and_drop_by_offset(slide2, -(pixels * predict), 0).perform()
+        time.sleep(5)
+
+
+
+        # Turn the fpl_data into Panda DataFrame
+        data = pd.DataFrame(fpl_data)
+        data2 = pd.DataFrame(fpl_data2)
+
+        # Merge all the dataFrames using the Name column to match the data across dataFrames 
+        dataAll = data.merge(data2, how='left', on='Name')
+
+        # Fill all blanks in the dataframe with zeros so the results don't return NaN
+        dataAll.fillna(0, inplace=True)
+
+        # Print out the data in the terminal
+        print(dataAll)
+
+        # Turn the panda into an excel file
+        dataAll.to_excel('testing.xlsx', index = False)
+
+
+        # Get the current GW for while loop
         current_gw = driver.find_element(by=By.XPATH, value='//*[@id="__next"]//div[2]/div[2]/span/span[4]/span/span/span').text
+        time.sleep(5)
+
+        # Move lower slider to appropraite GW for the scrapping
+        ActionChains(driver).drag_and_drop_by_offset(slide2, pixels, 0).perform()
+        time.sleep(5)
+        ActionChains(driver).drag_and_drop_by_offset(slide1, pixels, 0).perform()
+        time.sleep(5)
+
+
 
 
 main()

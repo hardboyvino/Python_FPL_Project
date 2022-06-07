6257435436131614fcfd2a238f1af73e859dfce4
hardboyvino <97181625+hardboyvino@users.noreply.github.com>
2022-06-07 20:38:02 +0100

    Update splitter.py

--- FPL/splitter.py
+++ FPL/splitter.py
@@ -6,12 +6,13 @@ filename = "201819 PerApp GK2.csv"
 
 # Read the file
 dataset = pd.read_csv(filename)
+column = "Cost (M)"
 
 # Turn this particular column into a float
-dataset["Cost (M)"] = dataset["Cost (M)"].astype(float)
+dataset[column] = dataset[column].astype(float)
 
 # If the column matches the criteria, extract it. Else, leave it alone
-dataset = dataset.loc[dataset["Cost (M)"] > 5]
+dataset = dataset.loc[dataset[column] > 5]
 
 # Export the extract to another csv that can be used later
 dataset.to_csv(f"{filename} - splitter.csv", mode="a",index=False)
